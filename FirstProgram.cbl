295b10da2f52665d2b2380fad501fa016495315e
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2025-03-07 23:55:46 +0100

    Update FirstProgram.cbl

--- FirstProgram.cbl
+++ FirstProgram.cbl
@@ -6,7 +6,7 @@
        WORKING-STORAGE SECTION.
        01  WS-NOM PIC X(12) VALUE 'RAPHAEL'.
            02 WS-AGE PIC 9(3).
-           03 SOLDE PIC 9(4).
+           03 SOLDE PIC 9(4)V99.
            88 WS-MAJEUR VALUE 18.
 
        PROCEDURE DIVISION.
@@ -16,7 +16,7 @@
 
            ACCEPT WS-NOM.
 
-           MOVE 1457 TO SOLDE.
+           MOVE 1457.59 TO SOLDE.
            DISPLAY SOLDE.
 
            DISPLAY "Et bien salut " WS-NOM.
